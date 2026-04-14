3067069e274949ec3ab927f575b082e43cc89457
ThibaultGM78 <thibault.garciam@gmail.com>
2026-04-14 20:22:06 +0200

    clean

--- backend/src/main/java/bank/application/dao/IVirementDAO.java
+++
@@ -1,8 +0,0 @@
-package bank.application.dao;
-
-import bank.application.domain.Compte;
-import bank.application.domain.Virement;
-
-public interface IVirementDAO {
-    void sauvegarder(Virement virement);
-}
--- backend/src/main/java/bank/application/usecase/IExecuterVirementUseCase.java
+++
@@ -1,8 +0,0 @@
-package bank.application.usecase;
-
-import bank.adapter.in.web.dto.VirementDTO;
-import bank.application.domain.Virement;
-
-public interface IExecuterVirementUseCase {
-    void executeTransfer(Virement virement);
-}
